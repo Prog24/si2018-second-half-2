65234cccff59e2a68c1380bde34a2cb87f768b1a
Prog24 <prog24.net@gmail.com>
2018-09-13 17:16:22 +0900

    merge

--- controllers/usertempmatch/user_temp_match.go
+++ controllers/usertempmatch/user_temp_match.go
@@ -16,15 +16,129 @@ import (
 func GetTempMatch(p si.GetTempMatchParams) middleware.Responder {
 	s := repositories.NewSession()
 
-	// Validation
 	t := p.Token
 	if res := ValidateGetTempMatch(s, t); res != nil {
 		return res
 	}
 
-	//
+	me, err := tokenlib.GetUserByToken(s, t)
+	if err != nil {
+		return si.NewGetTempMatchInternalServerError().WithPayload(
+			&si.GetTempMatchInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error :: Meの取得に失敗しました",
+			})
+	}
+	if me == nil {
+		return si.NewGetTempMatchUnauthorized().WithPayload(
+			&si.GetTempMatchUnauthorizedBody{
+				Code:    "401",
+				Message: "Unauthorized :: Tokenが無効です",
+			})
+	}
+	// temp_waitのis_matchedをみて、
+	waitR := repositories.NewUserWaitTempMatchRepository(s)
 
-	return si.NewGetTempMatchOK()
+	// 男性のマッチングが0の時 -> 400 Error
+	if me.GetOppositeGender() == `M` {
+		matchR := repositories.NewUserMatchRepository(s)
+		matchCount, err := matchR.FindAllByUserID(me.ID)
+		if err != nil {
+			return si.NewGetTempMatchInternalServerError().WithPayload(
+				&si.GetTempMatchInternalServerErrorBody{
+					Code:    "500",
+					Message: "Internal Server Error",
+				})
+		}
+		if matchCount != nil {
+			return si.NewGetTempMatchBadRequest().WithPayload(
+				&si.GetTempMatchBadRequestBody{
+					Code:    "400",
+					Message: "Bad Request",
+				})
+		}
+	}
+
+	/** 今日すでにマッチングしたかどうかに関してはGETでは考慮する必要ない。。 **/
+
+	// アクティブがあるかどうかの確認
+	isActive, err := waitR.IsIActive(*me)
+	if err != nil {
+		return si.NewGetTempMatchInternalServerError().WithPayload(
+			&si.GetTempMatchInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	}
+	if !isActive {
+		// アクティブが無い -> BAD REQUEST
+		return si.NewGetTempMatchBadRequest().WithPayload(
+			&si.GetTempMatchBadRequestBody{
+				Code:    "400",
+				Message: "Bad Request",
+			})
+	}
+	// アクティブなレコードが存在する
+	// temp_matchを見に行って、 => 自分のIDを含む有効なレコードが存在するかチェック
+	tempMatchR := repositories.NewUserTempMatchRepository(s)
+	tempMatchEnt, err := tempMatchR.GetByUserID(me.ID)
+	if err != nil {
+		return si.NewGetTempMatchInternalServerError().WithPayload(
+			&si.GetTempMatchInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	}
+	if tempMatchEnt != nil {
+		// => 有効な temp_match 存在する   -> 既にマッチングしていたから、それを返す
+		// TODO: wait テーブルの is_matched をTRUEに変更する
+		response := tempMatchEnt.Build()
+		return si.NewGetTempMatchOK().WithPayload(&response)
+	}
+	// => 有効な temp_match 存在しない -> マッチングしていないから、サーチ処理を行う -> サーチ結果での処理分岐
+	partnerID, err := waitR.SearchPartner(*me)
+	if err != nil {
+		return si.NewGetTempMatchInternalServerError().WithPayload(
+			&si.GetTempMatchInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	}
+	if partnerID == 0 {
+		// Partnerが見つからなかった -> 空を返す
+		responseEnt := entities.UserTempMatch{}
+		response := responseEnt.Build()
+		return si.NewGetTempMatchOK().WithPayload(&response)
+	}
+
+	// サーチ結果Partnerが見つかった
+	tempMatch := entities.UserTempMatch{
+		UserID: me.ID,
+		PartnerID: partnerID,
+		CreatedAt: strfmt.DateTime(time.Now()),
+		UpdatedAt: strfmt.DateTime(time.Now()),
+	}
+	err = tempMatchR.Create(tempMatch)
+	if err != nil {
+		return si.NewGetTempMatchInternalServerError().WithPayload(
+			&si.GetTempMatchInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	}
+
+	// TODO: wait テーブルの is_matched をTRUEに変更する
+
+	responseEnt, err := tempMatchR.GetByUserID(me.ID)
+	if err != nil {
+		return si.NewGetTempMatchInternalServerError().WithPayload(
+			&si.GetTempMatchInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error",
+			})
+	}
+	response := responseEnt.Build()
+	return si.NewGetTempMatchOK().WithPayload(&response)
 }
 
 func PostTempMatch(p si.PostTempMatchParams) middleware.Responder {
--- repositories/user_temp_match_repository.go
+++ repositories/user_temp_match_repository.go
@@ -3,6 +3,8 @@ package repositories
 import (
 	"github.com/eure/si2018-second-half-2/entities"
 	"github.com/go-xorm/builder"
+	"time"
+	"github.com/go-openapi/strfmt"
 )
 
 type UserTempMatchRepository struct {
@@ -34,4 +36,37 @@ func (r *UserTempMatchRepository) Get(userID, partnerID int64) (*entities.UserTe
 		return &ent, nil
 	}
 	return nil, nil
+}
+
+// me_idで有効なレコードを探す
+// 有効なレコードとは？？
+func (r *UserTempMatchRepository) GetByUserID(userID int64) (*entities.UserTempMatch, error) {
+	var ent = entities.UserTempMatch{}
+
+	now := time.Now()
+	startTime := strfmt.DateTime(time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local))
+	endTime := strfmt.DateTime(time.Date(now.Year(), now.Month(), now.Day(), 23, 59, 59, 99, time.Local))
+
+	s := r.GetSession()
+
+	has, err := s.
+		Where("user_id = ?", userID).
+		Or("partner_id = ?", userID).
+		And("created_at > ?", startTime).
+		And("created_at < ?", endTime).
+		And("is_matched = ?", false).
+		Desc("created_at").
+		Limit(1, 0).
+		Get(&ent)
+
+	if err != nil {
+		return nil, err
+	}
+	if has {
+		return &ent, nil
+	}
+
+	return nil, nil
+
+	//return &ent, err
 }
