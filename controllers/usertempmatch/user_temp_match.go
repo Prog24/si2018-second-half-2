6effaf8d69038c042f5bcb2abe4c927ab1badf56
scizorman <tueda1207@gmail.com>
2018-09-14 11:38:57 +0900

    <tempmatch:POST> added function: partner's UserWaitTempMatch.IsMatched -> true

--- controllers/usertempmatch/user_temp_match.go
+++ controllers/usertempmatch/user_temp_match.go
@@ -149,6 +149,33 @@ func GetTempMatch(p si.GetTempMatchParams) middleware.Responder {
 			})
 	}
 
+	// TODO: 相手の UserWaitTempMatch の is_matched -> true
+	updatedPartnerWaitEnt, err := waitRepo.GetLatestByUserID(tempmatchEnt.PartnerID)
+	if err != nil {
+		return si.NewPostTempMatchInternalServerError().WithPayload(
+			&si.PostTempMatchInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error :: Failed to get partner's UserWaitTempMatch " + err.Error(),
+			})
+	}
+	if updatedPartnerWaitEnt == nil {
+		return si.NewPostTempMatchBadRequest().WithPayload(
+			&si.PostTempMatchBadRequestBody{
+				Code:    "400",
+				Message: "Bad Request :: Failed to get partner's UserWaitTempMatch",
+			})
+	}
+
+	updatedPartnerWaitEnt.IsMatched = true
+	err = waitRepo.Update(updatedPartnerWaitEnt)
+	if err != nil {
+		return si.NewPostTempMatchInternalServerError().WithPayload(
+			&si.PostTempMatchInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error :: Failed to change partner's is_matched -> true",
+			})
+	}
+
 	// responseEnt, err := tempMatchR.GetByUserID(me.ID)
 	// if err != nil {
 	// 	return si.NewGetTempMatchInternalServerError().WithPayload(
@@ -335,6 +362,33 @@ func PostTempMatch(p si.PostTempMatchParams) middleware.Responder {
 			})
 	}
 	fmt.Println("after : update")
+
+	// TODO: 相手の UserWaitTempMatch の is_matched -> true
+	updatedPartnerWaitEnt, err := waitRepo.GetLatestByUserID(tempmatchEnt.PartnerID)
+	if err != nil {
+		return si.NewPostTempMatchInternalServerError().WithPayload(
+			&si.PostTempMatchInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error :: Failed to get partner's UserWaitTempMatch " + err.Error(),
+			})
+	}
+	if updatedPartnerWaitEnt == nil {
+		return si.NewPostTempMatchBadRequest().WithPayload(
+			&si.PostTempMatchBadRequestBody{
+				Code:    "400",
+				Message: "Bad Request :: Failed to get partner's UserWaitTempMatch",
+			})
+	}
+
+	updatedPartnerWaitEnt.IsMatched = true
+	err = waitRepo.Update(updatedPartnerWaitEnt)
+	if err != nil {
+		return si.NewPostTempMatchInternalServerError().WithPayload(
+			&si.PostTempMatchInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error :: Failed to change partner's is_matched -> true",
+			})
+	}
 
 	// sEnt := updatedWaitEnt.Build()
 	sEnt := tempmatchEnt.Build()
